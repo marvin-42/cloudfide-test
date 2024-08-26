46c65e268be6e1686086210bce19dcf56327f3b2
Philip <nowakf@gmail.com>
2024-08-26 10:36:11 +0200

    fix: remove fetch transations extra params

--- src/api/transactions.ts
+++ src/api/transactions.ts
@@ -6,26 +6,15 @@ interface Transaction {
   time: Date;
 }
 
-interface TransactionParams {
-  symbol: "ETH" | "BTC";
-  limit?: number;
-}
-
-export const fetchTransactions = async ({
-  symbol,
-  limit,
-}: TransactionParams): Promise<Array<Transaction[]>> => {
+export const fetchTransactions = async (): Promise<Array<Transaction>> => {
   const params = new URLSearchParams({
-    symbol,
+    symbol: "BTCUSDT",
   });
-  if (limit) {
-    params.append("limit", limit.toString());
-  }
 
-  const response = await fetch("https://api.binance.com/api/v3/trades");
+  const response = await fetch(`https://api.binance.com/api/v3/trades?${params}`);
   const json = await response.json();
-  if (!json.ok) {
-    throw new Error(json?.message ?? "Something went wrong");
+  if (!response.ok) {
+    throw new Error(json.message);
   }
   return json;
 };
