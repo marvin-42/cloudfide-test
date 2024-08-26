664869997175fd8da6d5cbddeea16e5048dafb3f
Philip <nowakf@gmail.com>
2024-08-26 11:05:29 +0200

    fix: app ui container bigger width

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
-        <Container>
+        <Container maxW="container.xl">
           <TransactionChart />
         </Container>
       </ChakraProvider>
